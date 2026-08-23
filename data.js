7896d7e4304caf01a1585d18c1b68802dae56eaa
pollirey <psmirnova9@yandex.ru>
2026-08-23 10:20:06 +0300

    Обновлены файлы data.html, data.js, index.html, style.css и добавлены новые изображения

--- data.js
+++ data.js
@@ -1,13 +1,27 @@
 
-const buttons = document.querySelectorAll('.product-button');
+// const buttons = document.querySelectorAll('.product-button');
+
+// buttons.forEach(button => {
+//     button.addEventListener('click' , () => {
+//         const product = button.closest(".product");
+//         const name = product.dataset.name;
+//         const price = product.dataset.price;
+
+//         console.log(`Товар ${name}`);
+//         console.log(`Цена ${price}`);
+//     })
+// }) 
+
+const buttons = document.querySelectorAll('.user');
+const resultElement = document.querySelector('.result');
 
 buttons.forEach(button => {
     button.addEventListener('click' , () => {
-        const product = button.closest(".product");
-        const name = product.dataset.name;
-        const price = product.dataset.price;
+        const id = button.dataset.id;
+        const name = button.textContent;
 
-        console.log(`Товар ${name}`);
-        console.log(`Цена ${price}`);
-    })
-}) 
+        console.log(`Выбран пользователь с ID: ${id}`);
+        console.log(`Имя: ${name}`);
+        resultElement.textContent = `Выбран пользователь: ${name} (ID: ${id})`;
+    });
+});
